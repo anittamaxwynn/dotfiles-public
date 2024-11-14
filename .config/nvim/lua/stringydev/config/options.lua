d51edb1a365743cc8dee3e6d39f9fff656a61c74
StringyDev <107803920+stringydev@users.noreply.github.com>
2024-11-14 15:34:52 +0000

    fix(git): updated gitconfig path

--- .config/nvim/lua/stringydev/config/options.lua
+++ .config/nvim/lua/stringydev/config/options.lua
@@ -5,8 +5,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
--- vim.opt.pumblend = 0
--- vim.opt.winblend = 0
+
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,14 +23,14 @@ config.colors = {
 	foreground = "#CBE0F0",
 	background = "#011423",
 	cursor_bg = "#47FF9C",
-	cursor_border = "None",
-	cursor_fg = "None",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
 	selection_bg = "#033259",
 	selection_fg = "#CBE0F0",
-
-	-- ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	-- brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
+
 config.keys = {
 	{
 		key = "+",
@@ -66,14 +66,11 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 40
 -- config.text_background_opacity = 0.75
-config.default_cursor_style = "BlinkingBlock"
-config.cursor_blink_rate = 800
 config.animation_fps = 120
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
 config.automatically_reload_config = true
 config.max_fps = 120
 config.enable_kitty_graphics = true
 config.window_close_confirmation = "NeverPrompt"
 config.dpi = 144.0
+
 return config
