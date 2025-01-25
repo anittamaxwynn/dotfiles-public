1c36c7455ea9c10eaab7be40c6390e60baf215d8
dbozbay <107803920+dbozbay@users.noreply.github.com>
2025-01-25 03:07:09 +0000

    xcode theme

--- nvim/dot-config/nvim/lua/dbozbay/plugins/lsp.lua
+++ nvim/dot-config/nvim/lua/dbozbay/plugins/lsp.lua
@@ -30,12 +30,12 @@ return {
 					-- Using Ruff's import organizer
 					disableOrganizeImports = true,
 				},
-				python = {
-					analysis = {
-						-- Ignore all files for analysis to exclusively use Ruff for linting
-						ignore = { "*" },
-					},
-				},
+				-- python = {
+				-- 	analysis = {
+				-- 		-- Ignore all files for analysis to exclusively use Ruff for linting
+				-- 		ignore = { "*" },
+				-- 	},
+				-- },
 			},
 		})
 
---
+++ nvim/dot-config/nvim/lua/dbozbay/plugins/xcode.lua
@@ -0,0 +1,8 @@
+return {
+  "arzg/vim-colors-xcode",
+  priority = 1000,
+  config = function()
+    vim.g.xcodedarkhc_match_paren_style = 1
+    vim.cmd.colorscheme("xcodedarkhc")
+  end,
+}
