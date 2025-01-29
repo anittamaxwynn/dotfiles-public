ded744c6450e89eca17b983b86370fca076ef326
dbozbay <107803920+dbozbay@users.noreply.github.com>
2025-01-29 15:27:06 +0000

    chore

--- nvim/dot-config/nvim/lua/dbozbay/plugins/xcode.lua
+++ nvim/dot-config/nvim/lua/dbozbay/plugins/xcode.lua
@@ -1,9 +1,9 @@
 return {
-  "arzg/vim-colors-xcode",
-  priority = 1000,
-  enabled = false,
-  -- -- config = function()
-  -- --   vim.g.xcodedarkhc_match_paren_style = 1
-  -- --   vim.cmd.colorscheme("xcodedarkhc")
-  -- end,
+	"arzg/vim-colors-xcode",
+	priority = 1000,
+	enabled = true,
+	config = function()
+		vim.g.xcodedarkhc_match_paren_style = 1
+		vim.cmd.colorscheme("xcodedarkhc")
+	end,
 }
