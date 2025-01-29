fb122ab6e84cea545a305ba14cad3b3c66a68c70
dbozbay <107803920+dbozbay@users.noreply.github.com>
2025-01-29 15:46:44 +0000

    gruvbox colors

--- nvim/dot-config/nvim/lua/dbozbay/plugins/gruvbox.lua
+++ nvim/dot-config/nvim/lua/dbozbay/plugins/gruvbox.lua
@@ -5,8 +5,11 @@ return {
 	config = function()
 		-- Optionally configure and load the colorscheme
 		-- directly inside the plugin declaration.
-		-- vim.g.gruvbox_material_enable_italic = true
-  --       vim.g.gruvbox_material_transparent_background = 1
-		-- vim.cmd.colorscheme("gruvbox-material")
+		vim.g.gruvbox_material_enable_italic = 1
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_transparent_background = 2
+    vim.g.gruvbox_material_float_style = 'dim'
+    vim.g.gruvbox_material_menu_selection_background = 'purple'
+		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
--- nvim/dot-config/nvim/lua/dbozbay/plugins/xcode.lua
+++ nvim/dot-config/nvim/lua/dbozbay/plugins/xcode.lua
@@ -1,7 +1,7 @@
 return {
 	"arzg/vim-colors-xcode",
 	priority = 1000,
-	enabled = true,
+	enabled = false,
 	config = function()
 		vim.g.xcodedarkhc_match_paren_style = 1
 		vim.cmd.colorscheme("xcodedarkhc")
