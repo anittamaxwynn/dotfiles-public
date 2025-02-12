5f6534cf3f4fce667d80ac0d0a5f243b32014fcf
dbozbay <107803920+dbozbay@users.noreply.github.com>
2025-02-12 12:38:06 +0000

    modularize lazydev and add snacks global

--- nvim/dot-config/nvim/lua/plugins/lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,5 @@
 return {
     "neovim/nvim-lspconfig",
-    dependencies = {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-            library = {
-                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-            },
-        },
-    },
-
     config = function()
         local capabilities = require("blink.cmp").get_lsp_capabilities()
         local lspconfig = require("lspconfig")
